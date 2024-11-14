5d28ad1fb40d17dd429d7e0960759050ec813251
RealSao <7373331948sth@gmail.com>
2024-11-14 15:47:05 -0600

    Implement OuterUnionWith and Complement function

--- table.py
+++ table.py
@@ -43,19 +43,88 @@ class RelationalTable:
 
     # Perform an outer union with another table
     def OuterUnionWith(self, other_table):
-        aligned_self = self.DataFrame.reindex(columns=self.IntegrationIDToColumnIndex.keys())
-        aligned_other = other_table.DataFrame.reindex(columns=other_table.IntegrationIDToColumnIndex.keys())
+        # Get all unique integration IDs
+        all_integration_ids = set(self.IntegrationIDToColumnIndex.keys()).union(other_table.IntegrationIDToColumnIndex.keys())
+        
+        # Align columns based on integration IDs
+        aligned_self = self.DataFrame.copy()
+        aligned_other = other_table.DataFrame.copy()
 
+        # Map columns to integration IDs
+        aligned_self.columns = [self.IntegrationIDToColumnIndex.get(col, col) for col in aligned_self.columns]
+        aligned_other.columns = [other_table.IntegrationIDToColumnIndex.get(col, col) for col in aligned_other.columns]
+
+        # Reindex DataFrames to have the same columns
+        aligned_self = aligned_self.reindex(columns=all_integration_ids)
+        aligned_other = aligned_other.reindex(columns=all_integration_ids)
+
+        # Concatenate DataFrames
         self.DataFrame = pd.concat([aligned_self, aligned_other], axis=0, ignore_index=True).fillna("")
 
-        # TODO: will need to be a little more complex to capture the column indices in the new dataframe
+        # Update integration ID mappings
         self.IntegrationIDToColumnIndex.update(other_table.IntegrationIDToColumnIndex)
 
     # Complement the DataFrame to ensure completeness of tuples
     def Complement(self):
-        self.DataFrame = self.DataFrame.applymap(lambda x: x if x != "" else np.nan)
-        # TODO: implement complementation from the paper
+        U_ou = self.DataFrame.copy()  # Outer unioned tuples
+        U_comp = U_ou.copy()
+        U_temp = pd.DataFrame(columns=U_comp.columns)
+
+        # Iterate until no changes are made
+        while not U_temp.equals(U_comp):
+            U_temp = U_comp.copy()
+            U_comp = pd.DataFrame(columns=U_comp.columns)
+
+            # Iterate over each tuple in U_temp
+            for index1, t_1 in U_temp.iterrows():
+                complement_count = 0
+
+                # Iterate over each tuple in U_ou
+                for index2, t_2 in U_ou.iterrows():
+                    R, complement_status = self.k(t_1, t_2)
+                    if complement_status:
+                        # Add the new tuple R to U_comp
+                        U_comp = pd.concat([U_comp, pd.DataFrame([R])], ignore_index=True)
+                        complement_count += 1
+
+                if complement_count == 0:
+                    # If no complements were found, add t_1 back to U_comp
+                    U_comp = pd.concat([U_comp, pd.DataFrame([t_1])], ignore_index=True)
+
+        # Update the DataFrame with the complemented tuples
+        self.DataFrame = U_comp
         print("Complement operation performed.")
+        
+    # Complement function k(t_1, t_2)
+    def k(self, t_1, t_2):
+        complement_status = True
+
+        # Check if t_1 and t_2 are complementable
+        for col in self.DataFrame.columns:
+            val1 = t_1[col]
+            val2 = t_2[col]
+
+            if pd.notna(val1) and pd.notna(val2):
+                if val1 != val2:
+                    # Cannot complement if common attributes differ
+                    complement_status = False
+                    break
+
+        if complement_status:
+            # Create a new tuple R by combining t_1 and t_2
+            R = {}
+            for col in self.DataFrame.columns:
+                val1 = t_1[col]
+                val2 = t_2[col]
+                if pd.notna(val1):
+                    R[col] = val1
+                elif pd.notna(val2):
+                    R[col] = val2
+                else:
+                    R[col] = np.nan
+            return R, True
+        else:
+            return None, False
 
 
     # Remove tuples that are subsumed by other tuples
